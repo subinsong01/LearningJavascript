ffa868512c7cc9fa7a724cd8dda1e53099233745
subinsong01 <haely97@naver.com>
2024-02-02 23:11:30 +0900

    Day2

---
+++ Day2/While.js
@@ -0,0 +1,14 @@
+// while
+
+let n = 0;
+
+while(n){
+  console.log(n);
+}
+//Do-While
+do {
+  console.log(n) // 일단 실행 
+} while (n); // 그 다음 조선을 확인 -> n = 0 이니깐, false로 반복 멈춤
+
+// While <-> Do while : 후자는 틀려도 한 번은 꼭 반복하고, while은 처음부터 틀리면 반복하지 않는다
+//for과 while의 차이 = for의 경우 배열데이터 혹은 객체데이터를 다루는 것에 특화되어 있음
